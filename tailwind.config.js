7c202569adba799babde5fff0fa98932d9f1038a
abhishekxmehta <157567248+abhishekxmehta@users.noreply.github.com>
2025-03-13 11:08:50 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}', // Include all files in the pages directory
+    './components/**/*.{js,ts,jsx,tsx}', // Include all files in the components directory
+  ],
   theme: {
     extend: {
       colors: {
-        gold: '#FFD700',
+        'navy-blue': '#101585', // Dark navy blue
+        'mocha-mousse': '#A47864', // Warm mocha color
+        'muted-rose': '#D58D8D', // Subtle pinkish rose
+        'verdant-green': '#31EC56', // Vibrant green
+        'sunny-yellow': '#FFD43A', // Bright yellow
+        'crisp-white': '#FFFFFF', // Clean white
+        gold: '#FFD700', // Custom golden color for text
       },
     },
   },

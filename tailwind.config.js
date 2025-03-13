a2a5d43ac8253e3ebde65b3fbc6f98bb52267f2f
abhishekxmehta <157567248+abhishekxmehta@users.noreply.github.com>
2025-03-13 11:11:00 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}', // Include all files in the pages directory
-    './components/**/*.{js,ts,jsx,tsx}', // Include all files in the components directory
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        'navy-blue': '#101585', // Dark navy blue
-        'mocha-mousse': '#A47864', // Warm mocha color
-        'muted-rose': '#D58D8D', // Subtle pinkish rose
-        'verdant-green': '#31EC56', // Vibrant green
-        'sunny-yellow': '#FFD43A', // Bright yellow
-        'crisp-white': '#FFFFFF', // Clean white
-        gold: '#FFD700', // Custom golden color for text
+        'navy-blue': '#101585',
+        'mocha-mousse': '#A47864',
+        'muted-rose': '#D58D8D',
+        'verdant-green': '#31EC56',
+        'sunny-yellow': '#FFD43A',
+        'crisp-white': '#FFFFFF',
+        gold: '#FFD700',
       },
     },
   },
-  plugins: [],
 };
+

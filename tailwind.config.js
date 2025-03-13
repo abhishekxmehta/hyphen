7331d03a4d357bdc7efcafbcc0b0e757b932d8b0
abhishekxmehta <157567248+abhishekxmehta@users.noreply.github.com>
2025-03-13 10:54:13 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navy-blue': '#101585',
+        'mocha-mousse': '#A47864',
+        'muted-rose': '#D58D8D',
+        'verdant-green': '#31EC56',
+        'sunny-yellow': '#FFD43A',
+        'crisp-white': '#FFFFFF',
+      },
+    },
   },
   plugins: [],
-}
-
+};

3cf3f1cd386e16e215815329b2011950a26c0958
abhishekxmehta <157567248+abhishekxmehta@users.noreply.github.com>
2025-03-13 10:58:51 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'navy-blue': '#101585',
-        'mocha-mousse': '#A47864',
-        'muted-rose': '#D58D8D',
-        'verdant-green': '#31EC56',
-        'sunny-yellow': '#FFD43A',
-        'crisp-white': '#FFFFFF',
+        gold: '#FFD700',
       },
     },
   },
-  plugins: [],
 };

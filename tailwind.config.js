5d1bc007036e40232ece9ab1fe6446fdacc0038d
abhishekxmehta <157567248+abhishekxmehta@users.noreply.github.com>
2025-03-13 11:05:51 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}', // Include all files in the pages directory
-    './components/**/*.{js,ts,jsx,tsx}', // Include all files in the components directory
-    './app/**/*.{js,ts,jsx,tsx}', // Include all files in the app directory (if using Next.js 13+)
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        gold: '#FFD700', // Add a custom gold color
+        gold: '#FFD700',
       },
     },
   },
